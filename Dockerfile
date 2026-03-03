8aede829e479274533ed732e10ef09ab8c04c827
Michael Masterson <mgmastersonjr@gmail.com>
2026-03-03 09:09:39 -0500

    Updating docker and adding spec for DO

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,44 @@
-# We use the latest Rust stable release as base image
-FROM rust:1.91.1
-
-# Let's switch out working directory to `app` (equivalent to `cd app`)
-# The `app` folder will be created for us by Docker in case it does 
-# not exist already
+FROM lukemathwalker/cargo-chef:latest-rust-1.91.0 AS chef
 WORKDIR /app
-
-# Install the required system dependencies of our linking configuration
 RUN apt update && apt install lld clang -y
 
-# Copy all file from our working environment to out Docker image
+FROM chef AS planner
 COPY . .
+# Compute a lock-like file for our project
+RUN cargo chef prepare --recipe-path recipe.json
 
-ENV SQLX_OFFLINE true
+# Builder Stage
+# We use the latest Rust stable release as base image
+FROM chef AS builder
+COPY --from=planner /app/recipe.json recipe.json
+# Build our project dependencies, not our application!
+RUN cargo chef cook --release --recipe-path recipe.json
+# Up to this point, if our dependency tree stays the saame,
+# all layers should be cached.
+COPY . .
+ENV SQLX_OFFLINE=true
 
-# Let's build out binary!
+# Build binary!
 # We'll use the release profile to make it fast!
 RUN cargo build --release
 
+# Runtime Stage
+FROM debian:bookworm-slim AS runtime
+
+WORKDIR /app
+
+RUN apt-get update -y \
+  && apt-get install -y --no-install-recommends openssl ca-certificates \
+  # Clean Up
+  && apt-get autoremove -y \
+  && apt-get clean -y \
+  && rm -rf /var/lib/apt/list/*
+
+# Copy the compiled binary from the builder environment
+COPY --from=builder /app/target/release/zero2prod zero2prod
+# We need the configuration file at runtime!
+COPY configuration configuration
+ENV APP_ENVIRONMENT=production
+
 # When `docker run` is executed, launch the binary!
-ENTRYPOINT ["./target/release/zero2prod"]
+ENTRYPOINT ["./zero2prod"]
--- src/configuration.rs
+++ src/configuration.rs
@@ -1,25 +1,38 @@
 //! src/configuration.rs
-
+use crate::environment::Environment;
 use secrecy::{ExposeSecret, SecretString};
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ConnectionSettings,
 }
 
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
     pub password: SecretString,
+    pub database_name: String,
+    pub connection: ConnectionSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct ConnectionSettings {
     pub port: u16,
     pub host: String,
-    pub database_name: String,
 }
 
 pub fn get_config() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let config_dir = base_path.join("configuration");
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+    let env_filename = format!("{}.yaml", environment.as_str());
     let settings = config::Config::builder()
-        .add_source(config::File::new("config.yaml", config::FileFormat::Yaml))
+        .add_source(config::File::from(config_dir.join("base.yaml")))
+        .add_source(config::File::from(config_dir.join(env_filename)))
         .build()?;
     settings.try_deserialize::<Settings>()
 }
@@ -31,8 +44,8 @@ impl DatabaseSettings {
                 "postgres://{}:{}@{}:{}/{}",
                 self.username,
                 self.password.expose_secret(),
-                self.host,
-                self.port,
+                self.connection.host,
+                self.connection.port,
                 self.database_name
             )
             .into(),
@@ -45,8 +58,8 @@ impl DatabaseSettings {
                 "postgres://{}:{}@{}:{}",
                 self.username,
                 self.password.expose_secret(),
-                self.host,
-                self.port
+                self.connection.host,
+                self.connection.port
             )
             .into(),
         )
